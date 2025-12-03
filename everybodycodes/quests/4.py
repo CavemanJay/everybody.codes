7a44215b3aeec70bd841c349ed933e0df3f364d5
Jay <cuevas0212@gmail.com>
2025-12-03 11:54:55 -0500

    4.3

--- everybodycodes/quests/4.py
+++ everybodycodes/quests/4.py
@@ -8,15 +8,19 @@ from ..utils import get_notes
 
 
 def part_one(s: str):
-    nail_lengths  = [int(x)for x in s.splitlines()]
-    smallest=min(nail_lengths)
-    return sum(x-smallest for x in nail_lengths)
+    nail_lengths = [int(x) for x in s.splitlines()]
+    smallest = min(nail_lengths)
+    return sum(x - smallest for x in nail_lengths)
 
 
-ex = """3
-4
-7
-8"""
+part_two = part_one
+
+
+def part_three(s: str):
+    nail_lengths = [int(x) for x in s.splitlines()]
+    return min((sum(abs(x - target) for x in nail_lengths)) for target in nail_lengths)
+
 
 ic(part_one(get_notes(1)))
-ic(part_one(get_notes(2)))
+ic(part_two(get_notes(2)))
+ic(part_three(get_notes(3)))
