cb5a1e6bda1b6ae7411d1feb3460e5d948b6c316
Jay <cuevas0212@gmail.com>
2025-12-03 01:00:14 -0500

    2.3 incomplete

--- everybodycodes/quests/2.py
+++ everybodycodes/quests/2.py
@@ -45,15 +45,53 @@ def part_two(s: str):
     )
 
 
-x = """
-WORDS:THE,OWE,MES,ROD,HER,QAQ
+def part_three(s: str):
+    runes, inscription = parse_input(s)
+    lines = list(line for line in inscription.splitlines())
+    # runes = ["RODEO"]
+    # lines = [lines[-1]]
+    coords = set()
+    for row, line in enumerate(lines):
+        ic(row,len(lines))
+        for col, char in enumerate(line):
+            for rune in runes:
+                # left to right
+                to_check = (
+                    line[col::] + line[0:col] if col + len(rune) > len(line) else line[col:]
+                )
+                if to_check.startswith(rune):
+                    coords.add( ( row, col, tuple( (row, col_index % len(line)) for col_index in range(col, len(rune) + col)), rune,))
 
-AWAKEN THE POWE ADORNED WITH THE FLAMES BRIGHT IRE
-THE FLAME SHIELDED THE HEART OF THE KINGS
-POWE PO WER P OWE R
-THERE IS THE END
-QAQAQ
+                # right to left
+                to_check = ''.join( line[(col - i) % len(line)] for i in range(len(line)))
+                if to_check.startswith(rune):
+                    coords.add( ( row, col, tuple( (row, col_index % len(line)) for col_index in (col-x for x in range(len(rune)))), rune,))
+
+                # top to bottom
+                to_check = "".join (line[col] for line in lines[row:])
+                if to_check.startswith(rune):
+                    coords.add( ( row, col, tuple((r,col) for r in range(len(rune))), rune,))
+                
+                # bottom to top
+                to_check = "".join (line[col] for line in lines[row::-1])
+                if to_check.startswith(rune):
+                    coords.add( ( row, col, tuple((r,col) for r in (row-x for x in range(len(rune)))), rune,))
+    return len(set(rune_coord for row,col,rune_coords,rune in coords for rune_coord in rune_coords))
+
+
+# 01234567
+# HELWORLT
+# ENIGWDXL
+# TRODEOAL
+
+x = """
+WORDS:THE,OWE,MES,ROD,RODEO
+
+HELWORLT
+ENIGWDXL
+TRODEOAL
 """
 
 ic(part_one(get_notes(1)))
 ic(part_two(get_notes(2)))
+ic(part_three(get_notes(3)))
