9b4254b944a4b1d1003cbcbd1b3128515fe7fcae
Jay <cuevas0212@gmail.com>
2025-12-03 11:34:39 -0500

    4.1

---
+++ everybodycodes/quests/4.py
@@ -0,0 +1,21 @@
+import itertools
+from pprint import pprint
+import re
+
+from icecream import ic
+
+from ..utils import get_notes
+
+
+def part_one(s: str):
+    nail_lengths  = [int(x)for x in s.splitlines()]
+    smallest=min(nail_lengths)
+    return sum(x-smallest for x in nail_lengths)
+
+
+ex = """3
+4
+7
+8"""
+
+ic(part_one(get_notes(1)))
