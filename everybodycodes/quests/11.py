be41de1ab517a504241ac7c27b5a0424d0df121c
Jay <cuevas0212@gmail.com>
2025-12-05 18:50:25 -0500

    11.2

--- everybodycodes/quests/11.py
+++ everybodycodes/quests/11.py
@@ -11,20 +11,29 @@ from icecream import ic
 from ..utils import get_notes, loop, print_aligned, transpose
 
 
-def part_one(s: str):
-    lookup = {k: v.split(",") for k, v in (line.split(":") for line in s.splitlines())}
-
-    gens = [Counter(["A"])]
-    for _ in range(4):
-        current = Counter()
-        prev = gens[-1]
+def gens(rules: str, start_gen: list[str]):
+    lookup = {
+        k: v.split(",") for k, v in (line.split(":") for line in rules.splitlines())
+    }
+    prev = Counter(start_gen)
+    for _ in itertools.count():
+        current: Counter[str] = Counter()
         for key, count in prev.items():
             assert isinstance(key, str)
             new = lookup[key] * count
             current.update(new)
-        gens.append(current)
+        prev = current
+        yield current
 
-    return sum(gens[-1].values())
+
+def part_one(s: str):
+    gen = 4
+    return sum(next(islice(gens(s, ["A"]), gen - 1, gen)).values())
+
+
+def part_two(s: str):
+    gen = 10
+    return sum(next(islice(gens(s, ["Z"]), gen - 1, gen)).values())
 
 
 ex1 = """A:B,C
@@ -33,3 +42,4 @@ C:A"""
 
 # ic(part_one(ex1))
 ic(part_one(get_notes(1)))
+ic(part_two(get_notes(2)))
