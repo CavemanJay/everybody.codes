487d4cc9cb3ddbfe9eafe8f7f24b478dfea2b7f2
Jay <cuevas0212@gmail.com>
2025-12-05 19:31:33 -0500

    11.3 Move from counter to defaultdict for performance

--- everybodycodes/quests/11.py
+++ everybodycodes/quests/11.py
@@ -1,45 +1,57 @@
-from collections import Counter
-from enum import IntFlag
-from functools import reduce
-from itertools import accumulate, count, cycle, groupby, islice, pairwise, permutations
+from collections import defaultdict
+from itertools import islice
 import itertools
-from math import factorial
-import math
-from pprint import pprint
-from typing import Callable
 from icecream import ic
-from ..utils import get_notes, loop, print_aligned, transpose
+from ..utils import get_notes
 
 
 def gens(rules: str, start_gen: list[str]):
     lookup = {
         k: v.split(",") for k, v in (line.split(":") for line in rules.splitlines())
     }
-    prev = Counter(start_gen)
+    prev = defaultdict(int)
+    for termite in start_gen:
+        prev[termite] += 1
+
     for _ in itertools.count():
-        current: Counter[str] = Counter()
+        current = defaultdict(int)
         for key, count in prev.items():
-            assert isinstance(key, str)
-            new = lookup[key] * count
-            current.update(new)
+            for termite in lookup[key]:
+                current[termite] += count
         prev = current
         yield current
 
 
+def population_size(s: str, gen: int, start_gen: "list[str]"):
+    return sum(next(islice(gens(s, start_gen), gen - 1, gen)).values())
+
+
 def part_one(s: str):
-    gen = 4
-    return sum(next(islice(gens(s, ["A"]), gen - 1, gen)).values())
+    return population_size(s, 4, ["A"])
 
 
 def part_two(s: str):
-    gen = 10
-    return sum(next(islice(gens(s, ["Z"]), gen - 1, gen)).values())
+    return population_size(s, 10, ["Z"])
+
+
+def part_three(s: str):
+    gen = 20
+    pop_sizes = [
+        population_size(s, gen, start_gen)
+        for start_gen in ([line.split(":")[0]] for line in s.splitlines())
+    ]
+    return max(pop_sizes) - min(pop_sizes)
 
 
 ex1 = """A:B,C
 B:C,A
 C:A"""
 
+ex2 = """A:B,C
+B:C,A,A
+C:A"""
+
 # ic(part_one(ex1))
 ic(part_one(get_notes(1)))
 ic(part_two(get_notes(2)))
+ic(part_three(get_notes(3)))
