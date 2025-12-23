e9182d0ce10ea3287539471c0ad08054d531ddea
Jay <cuevas0212@gmail.com>
2025-12-23 17:51:53 -0500

    1.2

--- everybodycodes/2025/quests/1.py
+++ everybodycodes/2025/quests/1.py
@@ -10,19 +10,29 @@ def parse_input(s: str):
     return names, instructions
 
 
-def part_one(s: str):
+def solve(s: str, loop=False):
     names, instructions = parse_input(s)
     curr = 0
     for ins in instructions:
         direction = 1 if ins[0] == "R" else -1
         amount = int(ins[1:])
         curr += direction * amount
-        if curr < 0:
-            curr = 0
-        if curr >= len(names):
-            curr = len(names) - 1
-
+        if not loop:
+            if curr < 0:
+                curr = 0
+            if curr >= len(names):
+                curr = len(names) - 1
+        else:
+            curr = curr % len(names)
     return names[curr]
+
+
+def part_one(s: str):
+    return solve(s)
+
+
+def part_two(s: str):
+    return solve(s, True)
 
 
 ex1 = """Vyrdax,Drakzyph,Fyrryn,Elarzris
@@ -30,3 +40,4 @@ ex1 = """Vyrdax,Drakzyph,Fyrryn,Elarzris
 R3,L2,R3,L1"""
 
 ic(part_one(get_notes(1)))
+ic(part_two(get_notes(2)))
