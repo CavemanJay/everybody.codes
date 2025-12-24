8b066460b15d1b94e2457cf321a59897237240d1
Jay <cuevas0212@gmail.com>
2025-12-24 15:36:40 -0500

    5.3 incomplete

--- everybodycodes/2025/quests/5.py
+++ everybodycodes/2025/quests/5.py
@@ -1,7 +1,14 @@
+from itertools import takewhile
+from typing import Sequence
 from icecream import ic
+from functools import cmp_to_key
 from everybodycodes.utils import get_notes
 
 OptInt = int | None
+
+
+def from_digits(l: Sequence[OptInt]):
+    return int("".join(str(d) for d in l if d is not None))
 
 
 def build_fishbone(s: str):
@@ -27,20 +34,51 @@ def build_fishbone(s: str):
             spine.append(n)
             levels.append([None, n, None])
 
-    return id, levels, spine
+    return int(id), levels, from_digits(spine)
 
 
 def part_one(s: str):
     _, _, spine = build_fishbone(s)
-    return "".join(str(d) for d in spine)
+    return spine
 
 
 def part_two(s: str):
-    bone_scores = [
-        int("".join(str(d) for d in spine))
-        for _, _, spine in map(build_fishbone, s.splitlines())
-    ]
+    bone_scores = [spine for _, _, spine in map(build_fishbone, s.splitlines())]
     return max(bone_scores) - min(bone_scores)
+
+
+def part_three(s: str):
+    bone_scores = [
+        {"id": id, "levels": levels, "quality": quality}
+        for id, levels, quality in map(build_fishbone, s.splitlines())
+    ]
+
+    def sort_cmp(b1, b2):
+        q1 = b1["quality"]
+        q2 = b2["quality"]
+        l1 = b1["levels"]
+        l2 = b2["levels"]
+        i1 = b1["id"]
+        i2 = b2["id"]
+        if q1 > q2:
+            return -1
+        elif q1 < q2:
+            return 1
+
+        assert q1 == q2
+        differing = next(((x, y) for x, y in zip(l1, l2) if x != y), None)
+
+        if differing is None:
+            return 1 if i1 > i2 else -1
+
+        d1 = from_digits(differing[0])
+        d2 = from_digits(differing[1])
+        assert d1 != d2
+
+        return 1 if d1 < d2 else -1
+
+    ordered = sorted(bone_scores, key=cmp_to_key(sort_cmp))
+    return sum(x * y for x, y in enumerate([x["id"] for x in ordered], 1))
 
 
 ex1 = "58:5,3,7,8,9,10,4,5,7,8,8"
@@ -56,7 +94,27 @@ ex2 = """1:2,4,1,1,8,2,7,9,8,6
 9:5,7,7,3,7,2,3,8,6,7
 10:4,1,9,3,8,5,4,3,5,5"""
 
-ic(part_one(ex1))
+ex3 = """1:7,1,9,1,6,9,8,3,7,2
+2:6,1,9,2,9,8,8,4,3,1
+3:7,1,9,1,6,9,8,3,8,3
+4:6,1,9,2,8,8,8,4,3,1
+5:7,1,9,1,6,9,8,3,7,3
+6:6,1,9,2,8,8,8,4,3,5
+7:3,7,2,2,7,4,4,6,3,1
+8:3,7,2,2,7,4,4,6,3,7
+9:3,7,2,2,7,4,1,6,3,7"""
+
+ex4 = """7:3,7,2,2,7,4,4,6,3,1
+8:3,7,2,2,7,4,4,6,3,7
+"""
+
+ex5 = """2:7,1,9,1,6,9,8,3,7,2
+1:7,1,9,1,6,9,8,3,7,2"""
+
+ex6 = """1:5,3,7,8,1,10,9,5,7,8
+2:5,3,7,8,1,10,9,4,7,9"""
+
 ic(part_one(get_notes(1)))
-ic(part_two(ex2))
 ic(part_two(get_notes(2)))
+ic(part_three(ex3))
+ic(part_three(get_notes(3)))
